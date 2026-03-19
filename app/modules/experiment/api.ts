5da7037c9ef5e29af8a0eafbd57ac307cc2ee7ef
M Haidar Hanif <git@mhaidarhanif.com>
2026-03-19 22:07:58 +0700

    day 37

--- app/modules/experiment/api.ts
+++ app/modules/experiment/api.ts
@@ -1,31 +1,10 @@
 import createFetchClient from "openapi-fetch";
 import createClient from "openapi-react-query";
+
 import type { paths } from "~/schema";
 
-const fetchClient = createFetchClient<paths>({ baseUrl: import.meta.env.VITE_API_URL });
+const fetchClient = createFetchClient<paths>({
+  baseUrl: import.meta.env.VITE_API_URL,
+});
 
 export const $api = createClient(fetchClient);
-
-// const { data, error } = await fetchClient.GET("/products");
-
-// await client.PUT("/products/{id}", {
-//   params: {
-//     path: { id: "123" },
-//   },
-//   body: {
-//     name: "My New Post",
-//     slug: "my-new-post",
-//     price: 10000,
-//     imageUrl: "https://example.com/images/products/my-new-post.jpg",
-//     description: "My new post description",
-//     category: "post",
-//     stock: 10,
-//     tags: ["post", "new", "example"],
-//   },
-// });
-
-// export async function getProducts() {
-//   const { data: products, error } = await fetchClient.GET("/products");
-//   console.log(products, error);
-//   return { data, error };
-// }
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,9 +1,11 @@
 import { Homepage } from "~/modules/home/homepage";
 import type { Route } from "./+types/home";
-import { $api } from "~/modules/experiment/api";
 
 export function meta({}: Route.MetaArgs) {
-  return [{ title: "Machamacha Online Store" }, { name: "description", content: "Welcome to Machamacha Online Store!" }];
+  return [
+    { title: "Machamacha Online Store" },
+    { name: "description", content: "Welcome to Machamacha Online Store!" },
+  ];
 }
 
 export default function Home() {
