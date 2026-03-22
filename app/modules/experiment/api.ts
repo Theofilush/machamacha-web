b6f027f0fe6eface8b54e30591a3f451cbe5003c
Theofilus Handoyo <realth99@gmail.com>
2026-03-22 17:19:38 +0700

    feat: Add experiment api

--- app/modules/experiment/api.ts
+++ app/modules/experiment/api.ts
@@ -8,3 +8,27 @@ const fetchClient = createFetchClient<paths>({
 });
 
 export const $api = createClient(fetchClient);
+
+// const { data, error } = await fetchClient.GET("/products");
+
+// await client.PUT("/products/{id}", {
+//   params: {
+//     path: { id: "123" },
+//   },
+//   body: {
+//     name: "My New Post",
+//     slug: "my-new-post",
+//     price: 10000,
+//     imageUrl: "https://example.com/images/products/my-new-post.jpg",
+//     description: "My new post description",
+//     category: "post",
+//     stock: 10,
+//     tags: ["post", "new", "example"],
+//   },
+// });
+
+// export async function getProducts() {
+//   const { data: products, error } = await fetchClient.GET("/products");
+//   console.log(products, error);
+//   return { data, error };
+// }
--- app/modules/home/homepage.tsx
+++ app/modules/home/homepage.tsx
@@ -1,11 +1,7 @@
 import { $api } from "../experiment/api";
 
 export function Homepage() {
-  const {
-    data: products,
-    error,
-    isLoading,
-  } = $api.useQuery("get", "/products");
+  const { data: products, error, isLoading } = $api.useQuery("get", "/products");
 
   if (isLoading || !products) return <p>Loading products...</p>;
 
@@ -15,19 +11,12 @@ export function Homepage() {
     <>
       <section className="py-12">
         <div className="container mx-auto px-4 md:px-8 lg:px-16">
-          <h2 className="text-2xl font-bold mb-8 text-center">
-            OUR BEST-SELLING MATCHA
-          </h2>
+          <h2 className="text-2xl font-bold mb-8 text-center">OUR BEST-SELLING MATCHA</h2>
           <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
             {products.map((product) => (
-              <div
-                key={product.id}
-                className="border p-4 text-center dark:border-gray-600 dark:bg-gray-800"
-              >
+              <div key={product.id} className="border p-4 text-center dark:border-gray-600 dark:bg-gray-800">
                 <h3 className="font-semibold">{product.name}</h3>
-                <p className="text-green-600 dark:text-green-400">
-                  From Rp {product.price}
-                </p>
+                <p className="text-green-600 dark:text-green-400">From Rp {product.price}</p>
               </div>
             ))}
           </div>
@@ -40,30 +29,19 @@ export function Homepage() {
           <ul className="grid grid-cols-1 md:grid-cols-2 gap-6">
             <li className="p-4 bg-white shadow rounded dark:bg-gray-700">
               <h3 className="font-semibold">ANTIOXIDANT POWER</h3>
-              <p>
-                Matcha is like a cell superhero, keeping you healthy and
-                radiant!
-              </p>
+              <p>Matcha is like a cell superhero, keeping you healthy and radiant!</p>
             </li>
             <li className="p-4 bg-white shadow rounded dark:bg-gray-700">
               <h3 className="font-semibold">BRAINPOWER BOOST</h3>
-              <p>
-                Sharper focus with a side of calm. It’s like a chill pill and a
-                brain gym in a cup!
-              </p>
+              <p>Sharper focus with a side of calm. It’s like a chill pill and a brain gym in a cup!</p>
             </li>
             <li className="p-4 bg-white shadow rounded dark:bg-gray-700">
               <h3 className="font-semibold">METABOLISM MAGIC</h3>
-              <p>
-                Rev up your inner engine! Burn, baby, burn – in the best way!
-              </p>
+              <p>Rev up your inner engine! Burn, baby, burn – in the best way!</p>
             </li>
             <li className="p-4 bg-white shadow rounded dark:bg-gray-700">
               <h3 className="font-semibold">HEART’S BEST FRIEND</h3>
-              <p>
-                Like a cozy hug for your heart – keeping it strong and loved
-                with every sip.
-              </p>
+              <p>Like a cozy hug for your heart – keeping it strong and loved with every sip.</p>
             </li>
           </ul>
         </div>
@@ -71,22 +49,12 @@ export function Homepage() {
 
       <section className="py-12">
         <div className="container mx-auto px-4 md:px-8 lg:px-16">
-          <h2 className="text-2xl font-bold mb-8 text-center">
-            EXPLORE RECIPES
-          </h2>
+          <h2 className="text-2xl font-bold mb-8 text-center">EXPLORE RECIPES</h2>
           <ul className="grid grid-cols-1 md:grid-cols-4 gap-6 text-center">
-            <li className="border p-4 dark:border-gray-600 dark:bg-gray-800">
-              ICED MATCHA LATTE
-            </li>
-            <li className="border p-4 dark:border-gray-600 dark:bg-gray-800">
-              COCONUT MATCHA
-            </li>
-            <li className="border p-4 dark:border-gray-600 dark:bg-gray-800">
-              MATCHARICANO
-            </li>
-            <li className="border p-4 dark:border-gray-600 dark:bg-gray-800">
-              ICED HOJICHA LATTE
-            </li>
+            <li className="border p-4 dark:border-gray-600 dark:bg-gray-800">ICED MATCHA LATTE</li>
+            <li className="border p-4 dark:border-gray-600 dark:bg-gray-800">COCONUT MATCHA</li>
+            <li className="border p-4 dark:border-gray-600 dark:bg-gray-800">MATCHARICANO</li>
+            <li className="border p-4 dark:border-gray-600 dark:bg-gray-800">ICED HOJICHA LATTE</li>
           </ul>
         </div>
       </section>
