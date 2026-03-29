7e4b1dfab1423d7fef3c63712fea9198a9a04dfe
Theofilus Handoyo <realth99@gmail.com>
2026-03-30 00:00:16 +0700

    feat: implement cookie session storage and add required environment variables

---
+++ app/sessions.server.ts
@@ -0,0 +1,24 @@
+import { createCookieSessionStorage } from "react-router";
+
+type SessionData = {
+  token: string;
+};
+
+type SessionFlashData = {
+  error: string;
+};
+
+const { getSession, commitSession, destroySession } = createCookieSessionStorage<SessionData, SessionFlashData>({
+  cookie: {
+    name: "__session",
+    // expires: new Date(Date.now() + 60_000 * 60 * 24 * 7), // 7 days
+    httpOnly: true,
+    maxAge: 60 * 60 * 24 * 7,
+    path: "/",
+    sameSite: "lax",
+    secrets: [import.meta.env.COOKIE_SECRET_KEY],
+    secure: true,
+  },
+});
+
+export { getSession, commitSession, destroySession };
